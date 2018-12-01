a4be50f993517e936c4b5ee075bee0e2c898dda4
Jan de Vries <jan@jan-v.nl>
2018-12-01 14:49:46 +0100

    Initial commit

---
+++ FututreTech.CFPParser/Model/Excel/Cfp.cs
@@ -0,0 +1,24 @@
+﻿namespace FututreTech.CFPParser.Model.Excel
+{
+    internal class Cfp
+    {
+        public string Id { get; set; }
+        public string NameContactPerson { get; set; }
+        public string EmailContactPerson { get; set; }
+        public string NameSpeaker { get; set; }
+        public string EmailSpeaker { get; set; }
+        public string MobilePhoneSpeaker { get; set; }
+        public string ExtraNotesSpeaker { get; set; }
+        public string TitleTalk { get; set; }
+        public string DescriptionTalk { get; set; }
+        public string SessionType { get; set; }
+        public string Tracks { get; set; }
+        public string Difficulty { get; set; }
+        public string TargetGroupDeveloper { get; set; }
+        public string TargetGroupArchitect { get; set; }
+        public string TargetGroupManagement { get; set; }
+        public string TargetGroupDisruptor { get; set; }
+        public string TargetGroupExpert { get; set; }
+        public string ExtraNotesSession { get; set; }
+    }
+}
