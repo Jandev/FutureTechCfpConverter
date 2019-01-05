3970d6a9f5f475e5274e0216bd2e50bb01caa62a
Jan de Vries <jan@jan-v.nl>
2019-01-05 14:49:13 +0100

    created csv cfp model

--- FututreTech.CFPParser/Model/Csv/Cfp.cs
+++ FututreTech.CFPParser/Model/Csv/Cfp.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FututreTech.CFPParser.Model.Csv
+﻿namespace FututreTech.CFPParser.Model.Csv
 {
-    class Cfp
+    internal class Cfp
     {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string avatar { get; set; }
+        public string location { get; set; }
+        public string bio { get; set; }
+        public string talk_format { get; set; }
+        public string twitter { get; set; }
+        public string url { get; set; }
+        public string organization { get; set; }
+        public string shirt_size { get; set; }
+        public string title { get; set; }
+        public string @abstract { get; set; }
+        public string description { get; set; }
+        public string notes { get; set; }
+        public string audience_level { get; set; }
+        public string tags { get; set; }
+        public string rating { get; set; }
+        public string state { get; set; }
+        public string confirmed { get; set; }
+        public string created_at { get; set; }
+        public string additional_info { get; set; }
     }
 }
